24bf39e6999f2d6eaceead5b88f013162748f257
alvin sison <alvin_sison100@fiberhome.com>
2022-05-06 11:02:35 +0800

    Commit initial 001

--- account-service/src/main/java/com/microservice/account/service/AccountServiceApplication.java
+++ account-service/src/main/java/com/microservice/account/service/AccountServiceApplication.java
@@ -2,11 +2,13 @@ package com.microservice.account.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
--- account-service/src/main/java/com/microservice/account/service/controllers/MainController.java
+++ account-service/src/main/java/com/microservice/account/service/controllers/MainController.java
@@ -1,9 +1,13 @@
 package com.microservice.account.service.controllers;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 /**
  * Created by Default on 05/05/2022.
@@ -16,9 +20,27 @@ public class MainController {
     private String port;
 
     @GetMapping("/showPort")
+
     private String showPort(){
 
         return port;
+    }
+
+    @GetMapping("/showMain")
+    @HystrixCommand(fallbackMethod = "getDefault",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value="60")
+            }
+    )
+    private Optional<String> showMain(){
+        return Optional.ofNullable("Microservice : Acccounts");
+    }
+
+    @SuppressWarnings("unused")
+    String getDefault() {
+
+        return "Error Exception";
     }
 
 }
